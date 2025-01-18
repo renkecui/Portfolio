2f1ac3e4fbb823606cd39e317328b95bd2edc9d9
Renke <renkecui@gmail.com>
2025-01-18 09:47:31 -0500

    merge

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -6,16 +6,22 @@ export const Banner = () => {
   return (
     <section className="banner" id="home">
       <Container>
-        <div className="info">
-          <h1>Renke Cui</h1>
-          <h2>Software Developer</h2>
-          <p className="description">
-            I'm a student at Northeastern university majoring in Computer
-            Engineering and Computer Science with a minor in Business
-            administration. I enjoy the challenge of always learning something
-            new.
-          </p>
-        </div>
+        <Row className="align-items-center">
+          <Col xs={12} md={6} xl={7}>
+            <div className="info">
+              <h1>Renke Cui</h1>
+              <h2>Software Developer</h2>
+              <p className="description">
+                I'm a student at Northeastern university majoring in Computer
+                Engineering and Computer Science with a minor in Business
+                administration. I enjoy the challenge of always learning
+                something new.
+              </p>
+            </div>
+          </Col>
+
+          <Col xs={12} md={6} xl={7}></Col>
+        </Row>
       </Container>
     </section>
   );
